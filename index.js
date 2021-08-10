d9e96e91d3fea2affc758eacdf1c4875dffa9b17
Wilfred Bayudan <wilfredbayudan@gmail.com>
2021-08-10 11:07:37 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const blocksInFt = 264;
+const scuberHq = 42;
+
+function distanceFromHqInBlocks(location) {
+  return Math.abs(scuberHq - location);
+}
+
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * blocksInFt;
+}
+
+function distanceTravelledInFeet(a, b) {
+  return Math.abs(a - b) * blocksInFt;
+}
+
+function calculatesFarePrice(a, b) {
+  const travelledInFt = distanceTravelledInFeet(a, b);
+  let fare;
+    if (travelledInFt > 2500) {
+      fare = 'cannot travel that far';
+    } else if (travelledInFt > 2000) {
+      fare = 25;
+    } else if (travelledInFt > 400) {
+      fare = (travelledInFt - 400) * 0.02; 
+    } else {
+      fare = 0;
+    }
+    return fare;
+}
